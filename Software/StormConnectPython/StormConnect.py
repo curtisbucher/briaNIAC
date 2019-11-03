c7cb46b74a07f98856408a67e3661db305c52458
curtisbucher <cpbucher5@gmail.com>
2019-11-03 13:51:37 -0800

    Added minipro EEPROM burner software and project schematics

--- Software/StormConnectPython/StormConnect.py
+++ Software/StormConnectPython/StormConnect.py
@@ -13,89 +13,91 @@ import os
 POS = b'\x01'
 NEG = b'\x00'
 
+
 def read(start_addr, end_addr, file=None):
     """Commanding arduino to read data to `file` from ROM from [start_addr]
     to [end_addr].
     """
-    
-    ## Sending command to arduino uno, as well as 2, 8 bit addresses for start_addr and end_addr respectively
+
+    # Sending command to arduino uno, as well as 2, 8 bit addresses for start_addr and end_addr respectively
     start_addrA = start_addr//256
-    start_addrB = start_addr%256
-    
+    start_addrB = start_addr % 256
+
     end_addrA = end_addr//256
-    end_addrB = end_addr%256
-    
+    end_addrB = end_addr % 256
+
     ser.write([1, start_addrA, start_addrB, end_addrA, end_addrB])
 
-    ## Receiving data from arduino. The size of each transmission is sent in
-    ## first byte.
+    # Receiving data from arduino. The size of each transmission is sent in
+    # first byte.
     print("yup")
-    received = ser.read(size = end_addr - start_addr)
+    received = ser.read(size=end_addr - start_addr)
     print("yup2")
-          
-    ## Indicating that the CPU is ready or more data
+
+    # Indicating that the CPU is ready or more data
     ser.write(POS)
-    
-    ## Writing received data to file
+
+    # Writing received data to file
     received_list = list(received)
     print("Data: ", received_list)
-    
-    ## Converting to hex to write to file
+
+    # Converting to hex to write to file
     received = [hex(x)[2:] for x in received]
     received = ' '.join(received)
-    
-    ##Converting to hex to write to file
+
+    # Converting to hex to write to file
     if file:
         with open(file, "w") as data:
             data.write(received)
-            
+
     return received_list
+
 
 def write(start_addr, end_addr, file):
     """Commanding arduino to write data from `file` into ROM from [start_addr]
     to [end_addr].
     """
-    
-    ## Sending 16 bit addresses and command
+
+    # Sending 16 bit addresses and command
     start_addrA = start_addr//256
-    start_addrB = start_addr%256
-    
+    start_addrB = start_addr % 256
+
     end_addrA = end_addr//256
-    end_addrB = end_addr%256
-    
-    ser.write([0, start_addrA, start_addrB , end_addrA, end_addrB])
-    
-    ## Text files are interpreted as hex values whereas other files are just sent as bytes
+    end_addrB = end_addr % 256
+
+    ser.write([0, start_addrA, start_addrB, end_addrA, end_addrB])
+
+    # Text files are interpreted as hex values whereas other files are just sent as bytes
     if ".txt" in file:
-        ## Reading data from file to write to ROM
+        # Reading data from file to write to ROM
         with open(file, "r") as d:
             data = d.read()
-            
-        ## Converting from hex string to int list
+
+        # Converting from hex string to int list
         data = data.split(' ')
-        data = [int(x,16) for x in data]
+        data = [int(x, 16) for x in data]
         data = data[0:end_addr - start_addr]
-        
+
     else:
-        ## Reading data from file to write to ROM
+        # Reading data from file to write to ROM
         with open(file, "rb") as d:
             data = d.read()
-    
-    ## Must be sent in batches of 64, then wait for conformation
+
+    # Must be sent in batches of 64, then wait for conformation
     print("\nSending Data...")
     print("-" * (((end_addr-start_addr)//64)+1))
     for a in range(start_addr, end_addr-64, 64):
         ser.write(data[a:a+64])
         ser.read()
-        print("*",end="")
-    
+        print("*", end="", flush=True)
+
     ser.write(data[a+64:end_addr])
     if ser.read() == NEG:
-        print("*\n")
+        print("*\n", flush=True)
 
-    ##Checking Data
+    # Checking Data
     received = read(start_addr, end_addr)
-    
+
     if received == data:
         accuracy = 100
     else:
@@ -104,17 +106,17 @@ def write(start_addr, end_addr, file):
             if received[x] != data[x]:
                 missed += 1
         accuracy = int((len(received) - missed)/len(received)*10000)/100
-                
+
     print("\nBytes Sent:", len(data))
     print("Accuracy:", str(accuracy)+"%")
-    
 
-## Creating Serial Connection
+
+# Creating Serial Connection
 try:
-    ser = serial.Serial('/dev/cu.usbmodem14201')##
-except: 
-    try: 
-        ser = serial.Serial('/dev/cu.usbmodem14101')##
+    ser = serial.Serial('/dev/cu.usbmodem14201')
+except:
+    try:
+        ser = serial.Serial('/dev/cu.usbmodem14101')
     except:
         raise(ConnectionError("Device not connected"))
 time.sleep(2)
@@ -123,7 +125,7 @@ os.system("clear")
 print("LightningStorm 0.3.0")
 print("--------------------")
 
-## Establishing Connection
+# Establishing Connection
 print("Connecting...")
 ser.write(POS)
 if ser.read() != POS:
@@ -131,23 +133,25 @@ if ser.read() != POS:
 print("Connected!\n")
 
 while True:
-    ## Getting user request
+    # Getting user request
     operation = input("Read/Write: ")
 
     if "read" in operation.lower():
-        start_addr = int(input("Start Address: ")) ## Add support for 16 bit addressing
+        # Add support for 16 bit addressing
+        start_addr = int(input("Start Address: "))
         end_addr = int(input("End Address: "))
         filename = input("File to read to: ")
-        
+
         read(start_addr, end_addr, filename)
-    
+
     elif "write" in operation.lower():
-        start_addr = int(input("Start Address: ")) ## Add support for 16 bit addressing
+        # Add support for 16 bit addressing
+        start_addr = int(input("Start Address: "))
         end_addr = int(input("End Address: "))
         filename = input("File to write from (.txt or other): ")
-        
+
         write(start_addr, end_addr, filename)
-        
+
     else:
         print("Ending...")
         break
